a176d1155cb1499728c29babf1e9dac127513204
wiggidywouter <wiggidywouter@yandex.com>
2015-11-20 12:44:33 +0100

    finished all assignments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -22,6 +22,7 @@ $(function() {
          * page?
          */
         it('are defined', function() {
+          console.log(allFeeds);
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -31,16 +32,33 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('have valid URLs', function () {
+           
+           for(index in allFeeds){
+            expect(allFeeds[index].url).toBeDefined();
+            expect(allFeeds[index].url).not.toBe('');
+           }
 
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('have valid name\'s', function () {
+           
+           for(index in allFeeds){
+            expect(allFeeds[index].name).toBeDefined();
+            expect(allFeeds[index].name).not.toBe('');
+           }
+
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+
+    describe('The menu', function () {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -48,14 +66,30 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
+      it('is hidden by default', function () {
+        expect(document.body.classList.contains('menu-hidden')).toBe(true);
+      });
+
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+      it('toggles visibility when menu-icon is clicked', function () {
+        document.querySelector('.menu-icon-link').click();
+        expect(document.body.classList.contains('menu-hidden')).toBe(false);
+        
+        document.querySelector('.menu-icon-link').click();
+        expect(document.body.classList.contains('menu-hidden')).toBe(true);
+      });
 
+    });
+
+    /* TODO: Write a new test suite named "Initial Entries" */
+    
+    describe('Initial Entries', function () {
+      
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -63,10 +97,49 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+      beforeEach(function(done) {
+        loadFeed(0,function() {
+          done();
+        });
+      });
+
+      it('exist', function (done) {
+        expect(document.querySelectorAll('.feed .entry').length).toBeGreaterThan(0);
+        done();
+      });
+      
+
+    });
+
+    /* TODO: Write a new test suite named "New Feed Selection"*/
+
+
+    describe('New Feed Selection', function () {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+      var firstFeedContent, secondFeedContent;
+
+      beforeEach(function(done) {
+        
+        loadFeed(0,function() {
+          
+          firstFeedContent = document.querySelector('.feed').innerHTML;
+          
+          loadFeed(1,function() {
+            secondFeedContent = document.querySelector('.feed').innerHTML;
+            done();
+          });
+
+        });
+
+      });
+
+      it('will change page content', function (done) {
+        expect(firstFeedContent).not.toEqual(secondFeedContent);
+        done();
+      });
+    });
 }());
